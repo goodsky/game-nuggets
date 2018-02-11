ad099c89b728a113aaee01d7be999446d07ff277
Skyler <goodsky6@gmail.com>
2018-02-11 11:48:50 -0800

    Tweak coloring on toolbar

---
+++ construction-engine/Assets/Scripts/UI/ButtonGroup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonGroup : MonoBehaviour
+{
+    public GameObject[] Buttons;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
