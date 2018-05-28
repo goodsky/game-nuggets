71d9de00a5372b3949e117067ce9f5787b1262eb
Skyler <goodsky6@gmail.com>
2018-05-27 18:33:17 -0700

    Path subimage and rotation is working!

--- construction-engine/Assets/Scripts/Campus/CampusPaths.cs
+++ construction-engine/Assets/Scripts/Campus/CampusPaths.cs
@@ -18,6 +18,7 @@ namespace Campus
         {
             _terrain = terrain;
             _path = new bool[_terrain.CountX, _terrain.CountZ];
+            SetupPathMapping();
         }
 
         /// <summary>
@@ -76,8 +77,8 @@ namespace Campus
         /// <summary>
         /// Update the material of the grid to look like the path.
         /// </summary>
-        /// <param name="x"></param>
-        /// <param name="z"></param>
+        readonly int[] dx = new[] { 0, 1, 0, -1 };
+        readonly int[] dz = new[] { 1, 0, -1, 0 };
         private void SetPathMaterial(int x, int z)
         {
             if (!_path[x, z])
@@ -86,8 +87,96 @@ namespace Campus
             }
             else
             {
-                _terrain.SetSubmaterial(x, z, 1);
+                // check the 4 adjacent squares to pick the correct image
+                int[] adj = new int[4];
+                for (int i = 0; i < 4; ++i)
+                {
+                    int checkX = x + dx[i];
+                    int checkZ = z + dz[i];
+                    adj[i] =
+                        (checkX > 0 && checkX < _terrain.CountX &&
+                         checkZ > 0 && checkZ < _terrain.CountZ &&
+                         _path[checkX, checkZ])
+                         ? 1 : 0;
+                }
+
+                _terrain.SetSubmaterial(
+                    x, 
+                    z, 
+                    _subMaterial[adj[0], adj[1], adj[2], adj[3]],
+                    _rotation[adj[0], adj[1], adj[2], adj[3]]);
             }
+        }
+
+        // mapping from adjacent paths to the material + rotation
+        // [top, right, bottom, left]
+        private int[,,,] _subMaterial;
+        private Rotation[,,,] _rotation;
+        private void SetupPathMapping()
+        {
+            // This is fun! Seemed like the best way to do it at the time. 
+            // Future people: let me know if I'm an idiot.
+
+            _subMaterial = new int[2, 2, 2, 2];
+            _rotation = new Rotation[2, 2, 2, 2];
+
+            // no adjacent ---
+            _subMaterial[0, 0, 0, 0] = 1;
+            _rotation   [0, 0, 0, 0] = Rotation.deg0;
+
+            // one adjacent ---
+            // top
+            _subMaterial[1, 0, 0, 0] = 2;
+            _rotation   [1, 0, 0, 0] = Rotation.deg0;
+            // right
+            _subMaterial[0, 1, 0, 0] = 2;
+            _rotation   [0, 1, 0, 0] = Rotation.deg90;
+            // bottom
+            _subMaterial[0, 0, 1, 0] = 2;
+            _rotation   [0, 0, 1, 0] = Rotation.deg180;
+            // left
+            _subMaterial[0, 0, 0, 1] = 2;
+            _rotation   [0, 0, 0, 1] = Rotation.deg270;
+
+            // two adjacent (angled) ---
+            // top & right
+            _subMaterial[1, 1, 0, 0] = 3;
+            _rotation   [1, 1, 0, 0] = Rotation.deg0;
+            // right & bottom
+            _subMaterial[0, 1, 1, 0] = 3;
+            _rotation   [0, 1, 1, 0] = Rotation.deg90;
+            // bottom & left
+            _subMaterial[0, 0, 1, 1] = 3;
+            _rotation   [0, 0, 1, 1] = Rotation.deg180;
+            // left & top
+            _subMaterial[1, 0, 0, 1] = 3;
+            _rotation   [1, 0, 0, 1] = Rotation.deg270;
+
+            // two adjacent (straight) ---
+            // top & bottom
+            _subMaterial[1, 0, 1, 0] = 4;
+            _rotation   [1, 0, 1, 0] = Rotation.deg0;
+            // right & left
+            _subMaterial[0, 1, 0, 1] = 4;
+            _rotation   [0, 1, 0, 1] = Rotation.deg90;
+
+            // three adjacent ---
+            // not left
+            _subMaterial[1, 1, 1, 0] = 5;
+            _rotation   [1, 1, 1, 0] = Rotation.deg0;
+            // not top
+            _subMaterial[0, 1, 1, 1] = 5;
+            _rotation   [0, 1, 1, 1] = Rotation.deg90;
+            // not right
+            _subMaterial[1, 0, 1, 1] = 5;
+            _rotation   [1, 0, 1, 1] = Rotation.deg180;
+            // not bottom
+            _subMaterial[1, 1, 0, 1] = 5;
+            _rotation   [1, 1, 0, 1] = Rotation.deg270;
+
+            // four adjacent ---
+            _subMaterial[1, 1, 1, 1] = 6;
+            _rotation   [1, 1, 1, 1] = Rotation.deg0;
         }
     }
 }
