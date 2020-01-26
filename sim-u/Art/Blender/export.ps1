75dc1fd074a1a8a5298d02cf74806353f8cf38ce
Skyler <goodsky6@gmail.com>
2020-01-26 08:23:40 -0800

    Clean up the small portable.

--- sim-u/Art/Blender/export.ps1
+++ sim-u/Art/Blender/export.ps1
@@ -18,8 +18,11 @@ if (!(Test-Path "$blenderPath"))
     exit
 }
 
+Write-Host "**************************************************************************"
 Write-Host "Exporting Blender assets to .FBX with Unity coordinate system corrections."
 Write-Host "Using Blender at path '$blenderPath'"
+Write-Host "**************************************************************************"
+Write-Host ""
 
 Push-Location $PSScriptRoot
 
@@ -37,9 +40,23 @@ $exportCount = 0
 $blenderFiles = Get-ChildItem -Path . -Filter $fileFilter -Recurse
 foreach ($blenderFile in $blenderFiles)
 {
-    Write-Host "Exporting '$blenderFile'..."
-
     $fullName = $blenderFile.FullName
+    $relativePath = $fullName.Substring($PSScriptRoot.Length + 1)
+    Write-Host "Exporting '$relativePath' -----------------------------------"
+
+    # The blender script also checks if the output file already exists.
+    # But it's faster to skip here. So we are checking twice.
+    $relativeDirectory = [System.IO.Path]::GetDirectoryName($relativePath)
+    $outputNoExt = [System.IO.Path]::GetFileNameWithoutExtension($relativePath)
+    $relativeOutput = [System.IO.Path]::Combine($relativeDirectory, $outputNoExt + ".fbx")
+    $outputFile = [System.IO.Path]::Combine($outputPath, $relativeOutput)
+
+    if ([System.IO.File]::Exists($outputFile))
+    {
+        Write-Host " [SKIPPING] Output '$relativeOutput' already exists! Delete file to re-export."
+        continue
+    }
+
     Invoke-Expression '& "$blenderPath" -b "$fullName" --python "$exportScript" -- "$outputPath"'
 
     $exportCount = $exportCount + 1
