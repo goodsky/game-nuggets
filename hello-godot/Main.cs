686ec754715941d53e45866dc85443783ec03ed1
Skyler <goodsky6@gmail.com>
2023-12-22 14:10:47 -0800

    Tweak: ramp up difficulty

--- hello-godot/Main.cs
+++ hello-godot/Main.cs
@@ -12,18 +12,32 @@ public partial class Main : Node
 	private static readonly string MusicName = "Music";
 	private static readonly string DeathSoundName = "DeathSound";
 
+	private const double InitialMobTimerWaitTime = 1.5;
+	private const double InitialMobMinVelocity = 100.0;
+	private const double InitialMobMaxVelocity = 200.0;
+
+	private const double MobTimerWaitTimeGrowthRate = 0.99;
+	private const double MobVelocityGrowthRate = 1.01;
+
 	[Export]
 	public PackedScene MobScene { get; set; }
 
 	private int _score;
 
-	public override void _Ready() {}
+	private double _mobTimerWaitTime;
+	private double _mobMinVelocity;
+	private double _mobMaxVelocity;
 
-	public override void _Process(double delta) {}
+	public override void _Ready() { }
+
+	public override void _Process(double delta) { }
 
 	public void NewGame()
 	{
 		_score = 0;
+		_mobTimerWaitTime = InitialMobTimerWaitTime;
+		_mobMinVelocity = InitialMobMinVelocity;
+		_mobMaxVelocity = InitialMobMaxVelocity;
 
 		GetTree().CallGroup("mobs", Node.MethodName.QueueFree);
 
@@ -50,8 +64,11 @@ public partial class Main : Node
 
 	private void OnStartTimerTimeout()
 	{
+		var mobTimer = GetNode<Timer>(MobTimerName);
+		mobTimer.WaitTime = _mobTimerWaitTime;
+		mobTimer.Start();
+
 		GetNode<Timer>(ScoreTimerName).Start();
-		GetNode<Timer>(MobTimerName).Start();
 
 		GetNode<AudioStreamPlayer>(MusicName).Play();
 	}
@@ -75,9 +92,16 @@ public partial class Main : Node
 		mob.Position = spawnLocation.Position;
 		mob.Rotation = rotation;
 
-		var velocity = new Vector2((float)GD.RandRange(150.0, 250.0), 0);
+		var velocity = new Vector2((float)GD.RandRange(_mobMinVelocity, _mobMaxVelocity), 0);
 		mob.LinearVelocity = velocity.Rotated(rotation);
 
 		AddChild(mob);
+
+		var mobTimer = GetNode<Timer>(MobTimerName);
+		mobTimer.WaitTime = _mobTimerWaitTime;
+
+		_mobTimerWaitTime *= MobTimerWaitTimeGrowthRate;
+		_mobMinVelocity *= MobVelocityGrowthRate;
+		_mobMaxVelocity *= MobVelocityGrowthRate;
 	}
 }
